f27b6ad47a8fd494fdfcfa6c31e5319720abdaa5
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-05-09 16:39:34 +0900

    axiosに切り替えたけどダメ

--- src/Pages/Reader.tsx
+++ src/Pages/Reader.tsx
@@ -1,15 +1,25 @@
+import axios from "axios"
 import { useSearchParams } from "react-router-dom"
 import { Fetcher } from "swr"
 import useSWR from 'swr'
 
-const imageFetcher: Fetcher<string> = (requestUrl: string) => fetch(requestUrl, {mode: 'no-cors', method: "GET"}).then(res => {
-        console.log(res)
-        return res.blob()
-    }).
-    then(blob => {
-        console.log(blob)
-        return window.URL.createObjectURL(blob)
-    })
+// const imageFetcher: Fetcher<string> = (requestUrl: string) => fetch(requestUrl, ).then(res => {
+//         console.log(res)
+//         return res.blob()
+//     }).
+//     then(blob => {
+//         console.log(blob)
+//         return window.URL.createObjectURL(blob)
+//     })
+
+const imageFetcher: Fetcher<string> = (requestUrl: string) => axios.create({
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
+}).get(requestUrl).then(res => {
+    console.log(res.data)
+    return window.URL.createObjectURL(res.data)
+})
 
 export default function Reader(){
     const [searchParams] = useSearchParams()
