7489ddaff71acf6dcba2bf57859ae44b9b31b75a
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-05-10 11:05:06 +0900

    formatを適用

--- client/src/Pages/Reader.tsx
+++ client/src/Pages/Reader.tsx
@@ -1,34 +1,35 @@
-import { useSearchParams } from "react-router-dom"
-import { Fetcher } from "swr"
+import { useSearchParams } from 'react-router-dom'
+import { Fetcher } from 'swr'
 import useSWR from 'swr'
 
-const imageFetcher: Fetcher<string> = (requestUrl: string) => fetch(`http://localhost:3000/api/image?src=${requestUrl}`, {
-}).then(res => {
-        console.log(res)
-        return res.blob()
-    }).
-    then(blob => {
-        console.log(blob)
-        return window.URL.createObjectURL(blob)
+const imageFetcher: Fetcher<string> = (requestUrl: string) =>
+  fetch(`http://localhost:3000/api/image?src=${requestUrl}`, {})
+    .then(res => {
+      console.log(res)
+      return res.blob()
+    })
+    .then(blob => {
+      console.log(blob)
+      return window.URL.createObjectURL(blob)
     })
 
-export default function Reader(){
-    const [searchParams] = useSearchParams()
-    const srcUrl = searchParams.get('src') ?? ''
+export default function Reader() {
+  const [searchParams] = useSearchParams()
+  const srcUrl = searchParams.get('src') ?? ''
 
-    const { data, error, isLoading } = useSWR(srcUrl, imageFetcher)
+  const { data, error, isLoading } = useSWR(srcUrl, imageFetcher)
 
-    if (error) {
-        console.log(error)
-        return <div>failed to load</div>
-    }
-    if (isLoading) return <div>loading...</div>
+  if (error) {
+    console.log(error)
+    return <div>failed to load</div>
+  }
+  if (isLoading) return <div>loading...</div>
 
-    return (
-        <>
-            <p>reader page</p>
-            <p>src: {srcUrl}</p>
-            <img src={data} />
-        </>
-    )
+  return (
+    <>
+      <p>reader page</p>
+      <p>src: {srcUrl}</p>
+      <img src={data} />
+    </>
+  )
 }
