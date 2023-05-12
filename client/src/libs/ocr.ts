2b4fbca6b543eb5f954c1e36e5a9c5889e9dbcb0
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-05-12 22:10:27 +0900

    vite-plugin-static-copyを使ってnewOCRClientを書いてみた

--- client/src/libs/ocr.ts
+++ client/src/libs/ocr.ts
@@ -1,6 +1,10 @@
 import { OCRClient } from 'node_modules/tesseract-wasm/dist'
 import { useEffect, useRef, useState } from 'react'
 
+const wasmPath = 'tesseract-wasm-files/tesseract-core.wasm'
+// const wasmFallbackPath = 'tesseract-wasm-files/tesseract-core-fallback.wasm'
+const workerPath = 'tesseract-wasm-files/tesseract-worker.js'
+
 export const useOCR = () => {
   const ocrClient = useRef<OCRClient>()
   const [imageBlob, setImageBlob] = useState<Blob>()
@@ -22,7 +26,7 @@ export const useOCR = () => {
     const imageBitmap = await createImageBitmap(imageBlob)
 
     if (!ocrClient.current) {
-      ocrClient.current = new OCRClient()
+      ocrClient.current = await newOCRClient()
 
       console.log('Fetching text recognition model')
       await ocrClient.current.loadModel(
@@ -41,6 +45,14 @@ export const useOCR = () => {
     } catch (err) {
       console.log(err)
     }
+  }
+
+  const newOCRClient = async () => {
+    // TODO: supportsFastBuildを使っての分岐
+    return new OCRClient({
+      wasmBinary: await (await fetch(wasmPath)).arrayBuffer(),
+      workerURL: workerPath
+    })
   }
 
   return { setImageBlob, ocrText }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/tesseract-wasm/dist/tesseract-core.wasm',
+          dest: 'tesseract-wasm-files'
+        },
+        {
+          src: 'node_modules/tesseract-wasm/dist/tesseract-core-fallback.wasm',
+          dest: 'tesseract-wasm-files'
+        },
+        {
+          src: 'node_modules/tesseract-wasm/dist/tesseract-worker.js',
+          dest: 'tesseract-wasm-files'
+        }
+      ]
+    })
+  ]
 })
